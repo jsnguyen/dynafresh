da85aefd89805a60495a40caeaa59ec55a608cff
jayke nguyen <jsn.phpro1701@gmail.com>
2020-10-12 01:01:19 -0700

    updated

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-var fs = require('fs');
-var express = require('express');
+var fs = require('fs')
+var path = require('path')
+var express = require('express')
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
+var app = express()
+app.use(express.static(__dirname + '/public'))
 console.log(__dirname+'/public')
 
-var hostname = '127.0.0.1';
-var port = 8131;
+var hostname = '127.0.0.1'
+var port = 8131
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
-var files = fs.readdirSync('public/images');
-console.log(files)
+function getImageArray(){
+  var files = fs.readdirSync('public/images')
+
+  var allowedExt = ['.jpg','.png','.pdf']
+
+  var imageFiles=[]
+  files.forEach(file => { 
+    if ( allowedExt.includes(path.extname(file)) ){
+      imageFiles.push(file)
+    }
+  }) 
+
+  return imageFiles
+}
+
+files = getImageArray()
 
 app.get('/response.json', bodyParser.json(), function (req, res) {
-  res.contentType('application/json');
-  res.send(JSON.stringify(files));
-});
+  res.contentType('application/json')
+  res.send(JSON.stringify(files))
+})
 
-app.listen(port, () => console.log('Server started on port: '+port));
-
+app.listen(port, () => console.log('Server started on port: '+port))
--- public/dynafresh.js
+++ public/dynafresh.js
@@ -1,46 +1,69 @@
-var timeoutPeriod = 500;
-var imageURI = 'images/plot.png';
-var x=0, y=0;
-var img = new Image();
-
-img.onload = function() {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    context.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
-    setTimeout(timedRefresh,timeoutPeriod);
-};
-
-function timedRefresh() {
+function timedRefresh(img) {
     // just change src attribute, will always trigger the onload callback
-    img.src = imageURI + '?d=' + Date.now();
+    img.src = img.src.split('?')[0] + '?d=' + Date.now()
 }
 
 function addFilename(arr) {
-  var fs = document.getElementById("fileSelect");
+  var fs = document.getElementById('fileSelect')
   arr.forEach( element => {
-    var option = document.createElement("option");
-    option.text = element; 
-    fs.add(option, fs[0]);
-  });
+    var option = document.createElement('option')
+    option.text = element 
+    fs.add(option)
+  })
+  fs.selectedIndex = '0'
 }
 
-function getJSON(url,updateCallback){
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', url , true);
+function getJSON(img,url,updateCallback){
+  var xmlhttp = new XMLHttpRequest()
+  xmlhttp.open('GET', url , true)
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4) {
       if(xmlhttp.status == 200) {
-        var json_obj = JSON.parse(xmlhttp.responseText);
-        console.log(json_obj);
-        updateCallback(json_obj);
+        var json_obj = JSON.parse(xmlhttp.responseText)
+        console.log(json_obj)
+        updateCallback(img,json_obj)
       }
     }
-  };
-  xmlhttp.timeout=5000;
-  xmlhttp.send(null);
+  }
+  xmlhttp.timeout=5000
+  xmlhttp.send(null)
 }
 
-getJSON("response.json", addFilename);
-img.src=imageURI;
+function setOnChange(img) {
+  var fs = document.getElementById('fileSelect')
+  fs.onchange = () => {
+    img.src = 'images/'+fs.options[fs.selectedIndex].text
+  }
+}
+
+function setDefaultPlot(img){
+  var fs = document.getElementById('fileSelect')
+  img.src = 'images/'+fs.options[0].text
+}
+
+function loadAll(img,json_obj){
+  addFilename(json_obj)
+  setDefaultPlot(img)
+}
+
+function main(){
+
+  var timeoutPeriod = 500
+  var x=0, y=0
+  var img = new Image()
+
+  img.onload = function() {
+      var canvas = document.getElementById('canvas')
+      var context = canvas.getContext('2d')
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+      context.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2)
+      setTimeout(() => timedRefresh(img),timeoutPeriod)
+  }
+
+  setOnChange(img)
+  getJSON(img,'response.json', loadAll)
+
+}
+
+main()
